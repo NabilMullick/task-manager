b77449ce0e12291d6c2072f260b5564e0effc7fb
Nabil Mullick <nabilmullick@example.com>
2025-04-14 20:49:53 +0530

    add dynamic URLs

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL, // ✅ this reads from your .env file
+  withCredentials: true,
 });
 export default api;
