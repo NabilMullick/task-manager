5d26fc96c4e86338e9ebed86f3be3e8a56324305
Nabil Mullick <nabilmullick@example.com>
2025-04-16 21:11:53 +0530

    update CORS configuration to allow all origins

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: "https://task-manager-seven-rosy.vercel.app/", // your Vercel domain
+  origin: "*", // your Vercel domain
   credentials: true,
 }));
 
